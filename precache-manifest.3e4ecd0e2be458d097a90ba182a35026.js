87cc055a8cb19cc800d38c48ec616149096ae402
AlexMartinLopez <133256423+AlexMartinLopez@users.noreply.github.com>
2024-09-26 20:18:53 +0200

    Updates

---
+++ precache-manifest.3e4ecd0e2be458d097a90ba182a35026.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d60d08ef4f1c010e3ddce42704c439b",
+    "url": "/gymshark/index.html"
+  },
+  {
+    "revision": "4b439c38dc6147c94a6a",
+    "url": "/gymshark/static/css/main.f7875dc0.chunk.css"
+  },
+  {
+    "revision": "6c7e6191f8496d255b26",
+    "url": "/gymshark/static/js/2.f75eb75b.chunk.js"
+  },
+  {
+    "revision": "2ad5599bf840057222563328d21f1846",
+    "url": "/gymshark/static/js/2.f75eb75b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e04fc65f6865c7cee0e",
+    "url": "/gymshark/static/js/3.1ef25a72.chunk.js"
+  },
+  {
+    "revision": "4b439c38dc6147c94a6a",
+    "url": "/gymshark/static/js/main.e9b3c0c0.chunk.js"
+  },
+  {
+    "revision": "f3ba74b31b2211e9ab75",
+    "url": "/gymshark/static/js/runtime-main.67dd76c4.js"
+  },
+  {
+    "revision": "24e208a397dd24b56a87afbcecad36b0",
+    "url": "/gymshark/static/media/Gymshark.24e208a3.png"
+  },
+  {
+    "revision": "e70f31d0b7bb088561e8d8296a82aba2",
+    "url": "/gymshark/static/media/cursos.e70f31d0.jpg"
+  },
+  {
+    "revision": "00528315ba35dd7e3673475d9f3c5476",
+    "url": "/gymshark/static/media/entre.00528315.jpg"
+  },
+  {
+    "revision": "7e063fe3f8c066424282466fa622751a",
+    "url": "/gymshark/static/media/entrenador.7e063fe3.jpg"
+  },
+  {
+    "revision": "985cc2b6724d04fef98240c2af36df48",
+    "url": "/gymshark/static/media/gif.985cc2b6.gif"
+  },
+  {
+    "revision": "54e57383d40dc97087356fb3e3f635d7",
+    "url": "/gymshark/static/media/gym.54e57383.jpg"
+  },
+  {
+    "revision": "97170924be4d516e15ec34faa975a0c3",
+    "url": "/gymshark/static/media/kickbox.97170924.jpg"
+  },
+  {
+    "revision": "f2495150a7fb1f14424d7064d11bb57c",
+    "url": "/gymshark/static/media/yoga.f2495150.jpg"
+  }
+]);
